6f09cf546ac3ce38956c69a87b49060092b63305
cpojer <christoph.pojer@gmail.com>
2026-01-14 09:11:12 +0900

    Update deps, use Oxfmt for sorting tailwind classes.

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,5 @@
 import nkzw from '@nkzw/eslint-config';
 import findWorkspaces from '@nkzw/find-workspaces';
-import eslintPluginBetterTailwindCSS from 'eslint-plugin-better-tailwindcss';
 import workspaces from 'eslint-plugin-workspaces';
 
 export default [
@@ -38,14 +37,11 @@ export default [
   },
   {
     plugins: {
-      'better-tailwindcss': eslintPluginBetterTailwindCSS,
       workspaces,
     },
     rules: {
       '@typescript-eslint/array-type': [2, { default: 'generic' }],
       '@typescript-eslint/no-explicit-any': 0,
-      'better-tailwindcss/enforce-consistent-class-order': 2,
-      'better-tailwindcss/no-conflicting-classes': 2,
       'import-x/no-extraneous-dependencies': [
         2,
         {
@@ -64,11 +60,6 @@ export default [
       ],
       'workspaces/no-absolute-imports': 2,
       'workspaces/no-relative-imports': 2,
-    },
-    settings: {
-      'better-tailwindcss': {
-        entryPoint: './example/client/src/App.css',
-      },
     },
   },
 ];
